ac5c7096685893748698d0d3430a6b0777736e4d
ementy <mariyan.todorov13@gmail.com>
2020-03-20 22:15:27 +0200

    minor changes

---
+++ src/LoLBoostring.WebApi.ViewModels/Orders/OrderOut.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using LoLBoosting.Entities;
+using LoLBoosting.Services.Mapping;
+
+namespace LoLBoosting.WebApi.ViewModels.Orders
+{
+    public class OrderOut : IMapFrom<Order>
+    {
+        public string ClientId { get; set; }
+        public string AccountUsername { get; set; }
+        public string AccountPassword { get; set; }
+    }
+}
